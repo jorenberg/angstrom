d97255c2eeb1db68a40188b4992fb38b20a77c9d
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-10-08 13:54:01 +0530

    Initial commit
    SEED®: Grunt based Build System.
    Path: config/tasks/compass.js
    Usage: $ npm install grunt-contrib-compass --save-dev

---
+++ config/tasks/compass.js
@@ -0,0 +1,24 @@
+/*!
+ * Ångström®
+ * ______________________________________________________________
+ * SEED®      : Grunt based Build System.
+ * ~~~~~
+ * Compass    : =================================================
+ * @require   : Ruby, Sass, and Compass >=1.0.1
+ * ______________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ______________________________________________________________
+ * @date      : 08-Oct-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : Grunt
+ * ______________________________________________________________
+ * Primarily Designed for WebKit Engine(s): -------------
+ * Safari — Version 10.0 (11602.1.50.0.10).
+ * Copyright © 2003 – 2016 Apple Inc. All rights reserved.
+ * ______________________________________________________________
+ */
