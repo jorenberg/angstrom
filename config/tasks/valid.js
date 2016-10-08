6d467255c68a3cd14000255ffc566892b55fb292
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-10-08 14:04:01 +0530

    Initial commit
    SEED®: Grunt based Build System.
    Path: config/tasks/valid.js
    Usage:
    1. $ npm install grunt-htmlhint --save-dev
    2. $ npm install grunt-scss-lint --save-dev
    3. $ npm install grunt-contrib-csslint --save-dev
    4. $ npm install grunt-contrib-jshint --save-dev

---
+++ config/tasks/valid.js
@@ -0,0 +1,24 @@
+/*!
+ * Ångström®
+ * ______________________________________________________________
+ * SEED®      : Grunt based Build System.
+ * ~~~~~
+ * Validator  : =================================================
+ * grunt-htmlhint / grunt-scss-lint
+ * grunt-contrib-csslint / grunt-contrib-jshint
+ * ______________________________________________________________
+ * @require   : .htmlhintrc, .scss-lint.yml,
+ *              .csslintrc and .jshintrc
+ * ______________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ______________________________________________________________
+ * @date      : 08-Oct-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : Grunt
+ * ______________________________________________________________
+ */
