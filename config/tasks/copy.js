b2e566fca7396459248c1a3bdc75fa3438053f60
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-10-08 14:00:39 +0530

    Initial commit
    SEED®: Grunt based Build System.
    Path: config/tasks/copy.js
    Usage: $ npm install grunt-contrib-copy --save-dev

---
+++ config/tasks/copy.js
@@ -0,0 +1,20 @@
+/*!
+ * Ångström®
+ * ______________________________________________________________
+ * SEED®      : Grunt based Build System.
+ * ~~~~~
+ * Copy       : =================================================
+ * @require   : ./settings.js
+ * ______________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ______________________________________________________________
+ * @date      : 08-Oct-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : Grunt
+ * ______________________________________________________________
+ */
