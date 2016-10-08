c81fde2da35e7c5d0b3aec547236b25c39e6c520
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-10-08 23:09:19 +0530

    Initial commit
    Build: Örebro | 08-Oct-2016.

---
+++ index.js
@@ -0,0 +1,14 @@
+/*!
+ * Ångström®
+ * ____________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * ____________________________________________________________________
+ * @date      : 08-Oct-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * ____________________________________________________________________
+ */
